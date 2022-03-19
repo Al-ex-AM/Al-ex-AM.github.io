20fd9c3567d1e9304b2acaa1bacd763c1e50e2dd
Alex <alexanderacosta@uninorte.edu.co>
2022-03-19 22:03:29 +0000

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,30 @@
 function first() {
-  let html = '<button id="second" class="button" onclick="second()">Click Me</button>'
+  let html = '<button id="second" class="button" onclick="second()">Click Me Again</button>'
   let btn = document.getElementById("first");
   document.getElementById("invisible").innerText = "";
-  document.getElementById("text").innerText = "Te amo";
+  document.getElementById("text").innerText = `Mar, I love you. I know this is a really simple present but I still wanted to give you something that I've made with my own 'hands' sometime, so here it is.
+
+I hope you really enjoy going to the concer and hearing Morat live, you've been listening to their music since a long time and I know you really enjoy listening, so seeing them perform could be even better.
+
+I love you a lot, I don't even think I can put it in word. We've been dating since a long time and really enjoy being with you, it's great to have you by my side and I hope I can have you for the years to come. 
+
+Enjoy your birthday, and also you be having something more, you'll see ; ) .`;
+
   document.getElementById("btnPlace").insertAdjacentHTML("afterbegin", html);
   btn.remove();
-}
+};
 
 function second() {
+  let html = '<button id="third" class="button" onclick="third()">If You want</button>'
   let btn = document.getElementById("second");
-  let html = '<button id="third" class="button" onclick="third()">Click Me</button>'
-  document.getElementById("text").innerText = "ok";
+  document.getElementById("text").innerText = "Somos como la sal y el mar, tal para cual.";
   document.getElementById("btnPlace").insertAdjacentHTML("afterbegin", html);
   btn.remove();
-}
+};
+
 function third() {
-  let html = '<img src="purpleheart.svg" alt="shit">';
+  let html = '<img class="heart" src="purpleheart.svg" alt="shit">';
+  document.getElementById("text").innerText = "";
   document.getElementById("imgPlace").insertAdjacentHTML("afterbegin", html);
-}
+  document.getElementById("third").innerText = "More hearts"
+};
